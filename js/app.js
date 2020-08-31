cca8e4f1e33b7e020b9f0f203019f3500f3f852d
Tiny999 <salamiabbas3@gmail.com>
2020-08-31 22:19:09 +0100

    add-tasks-functionality

---
+++ js/app.js
@@ -0,0 +1,53 @@
+// Variables 
+const form = document.querySelector('#task-form');
+const tasksList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filer');
+const taskInput = document.querySelector('#task');
+
+
+// Load Event Listeners
+loadListeners();
+
+// Event Listeners 
+function loadListeners(){
+  form.addEventListener('submit', addTask);
+}
+
+
+//Functions
+function addTask(e){
+  e.preventDefault();
+
+  if(taskInput.value === ''){
+    alert("Add a Task");
+  } else {
+    // Create a li element
+
+    const li = document.createElement('li');
+
+    // add class
+    li.className = 'collection-item';
+
+    // create text node and append to li
+    li.appendChild(document.createTextNode(taskInput.value));
+
+    // create new link element
+    const link = document.createElement('a');
+
+    link.className = 'delete-item secondary-content';
+
+    link.innerHTML = '<i class="fa fa-remove fa-2x"></i>';
+
+    li.appendChild(link);
+
+    // Append li to UL
+    tasksList.appendChild(li);
+
+    // Clear the Input
+    taskInput.value = '';
+
+
+  }
+
+}
